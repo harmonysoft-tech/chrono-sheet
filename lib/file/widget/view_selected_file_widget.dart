a8a89a894af581bc81432c2c55b57077db83f171
Denis Zhdanov <denis.zhdanov@dkatalis.com>
2025-03-25 10:12:40 +0800

    more logs on navigation to file

--- lib/file/widget/view_selected_file_widget.dart
+++ lib/file/widget/view_selected_file_widget.dart
@@ -6,6 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../log/util/log_util.dart';
+import '../../util/snackbar_util.dart';
 
 final _logger = getNamedLogger();
 
@@ -15,8 +16,6 @@ class ViewSelectedFileWidget extends ConsumerWidget {
   Future<void> _viewFile(GoogleFile file, BuildContext context) async {
     final appUri = Uri.parse("google-sheets://docs.google.com/spreadsheets/d/${file.id}/edit");
     final browserUri = Uri.parse("https://docs.google.com/spreadsheets/d/${file.id}/edit");
-    final messenger = ScaffoldMessenger.of(context);
-    final l10n = AppLocalizations.of(context);
     try {
       // we experienced that canLaunchUrl() returns 'false' for all urls under android. That is due to the fact
       // that by default applications are not allowed to query information about other applications.
@@ -24,16 +23,26 @@ class ViewSelectedFileWidget extends ConsumerWidget {
       // and/or requesting 'android.permission.QUERY_ALL_PACKAGES' permission. However, it looks easier to
       // just try to launch and handle an exception (if any).
       // More details can be found at https://developer.android.com/training/package-visibility
-      await launchUrl(appUri);
-      return;
-    } catch (_) {
-      _logger.info("failed to open the sheet document '${file.name}' in the google sheets application");
-      try {
-        await launchUrl(browserUri);
-      } catch (_) {
-        _logger.info("failed to open the sheet document '${file.name}' in the browser");
-        messenger.showSnackBar(SnackBar(content: Text(l10n.errorCanNotOpenFile)));
+      _logger.info("trying to launch a gsheet using url '$appUri'");
+      final ok = await launchUrl(appUri);
+      _logger.info("the launch is successful in gsheet application: $ok");
+      if (ok) {
+        return;
       }
+    } catch (e, stack) {
+      _logger.info("failed to open the sheet document '${file.name}' in the google sheets application", e, stack);
+      await _launchInBrowser(browserUri, file, context);
+    }
+    await _launchInBrowser(browserUri, file, context);
+  }
+
+  Future<void> _launchInBrowser(Uri uri, GoogleFile file, BuildContext context) async {
+    try {
+      _logger.info("trying to launch a browser using url '$uri'");
+      await launchUrl(uri);
+    } catch (e2, stack2) {
+      _logger.info("failed to open the sheet document '${file.name}' in the browser", e2, stack2);
+      SnackBarUtil.showL10nMessage(context, _logger, (l10n) => l10n.errorCanNotOpenFile);
     }
   }
 
