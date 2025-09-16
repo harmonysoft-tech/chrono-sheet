e618bffa4a27d8325aa71285e45531f25f5e4ce3
denis.zhdanov <denzhdanov@gmail.com>
2025-09-16 13:53:32 +0800

    setting up integration tests on CI

--- integration_test/run-integration-tests.sh
+++ integration_test/run-integration-tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "copying test resources into the emulator"
-for file in $(ls ./test_common/resources); do
-  filePath=./test_common/$file
-  echo "copying file '$filePath' to the emulator"
-  adb push $filePath /data/local/tmp
+ROOT_TEST_RESOURCES_DIR=./test_common/resources
+for fileName in $(ls ROOT_TEST_RESOURCES_DIR); do
+  FILE=$ROOT_TEST_RESOURCES_DIR/$fileName
+  echo "copying file '$FILE' to the emulator"
+  adb push $FILE /data/local/tmp
 done
 
 flutter test integration_test
