7d7eb98b9bf0459aa56b90525d5627bc66b5f794
Denis Zhdanov <denis.zhdanov@tech.jago.com>
2025-09-16 11:50:20 +0800

    setting up integration tests on CI

--- integration_test/run-integration-tests.sh
+++ integration_test/run-integration-tests.sh
@@ -2,6 +2,7 @@
 
 echo "copying test resources into the emulator"
 for file in $(ls ./test_common/resources); do
+  echo "copying file '$file' to the emulator"
   adb push $file /data/local/tmp
 done
 
